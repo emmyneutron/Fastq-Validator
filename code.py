8841c9d527628ea15c90b487ee0f2b93d39462e3
Eman Abdelrazik <eman50371@student.must.edu.eg>
2019-05-04 00:01:01 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,3 @@
-
 #1- Create a class named SeqRead
 class SeqRead:
     def __init__(self, seqheader, seq, plusline, qual):
@@ -7,29 +6,30 @@ class SeqRead:
         self.plus = plus
         self.qual = qual
         
-     def qualine_errors(self):
+    def read_gz(self): #open &read compreesed file
+        filepath= ""
+
+        if filepath.endswith('.gz'):
+            self._file = gzip.open(filepath)
+        else:
+            self._file = open(filepath, 'rU')
+
+        self._currentLineNumber = 0
+
+    def next(self):
+        # Get Next Four Lines 
+        elemList = []
+        for i in range(4):
+            line = self._file.readline()
+            self._currentLineNumber += 1 ## increment file position
+            if line:
+                elemList.append(line.strip('\n'))
+            else: 
+                elemList.append(None)
+
+    def qualine_errors(self):            
          if len(self.qual) == len(self.seq):
             print('valid read'.format(self.qual))
-        else:
-            print("Invalid read".format(self.qual))
-    
- 
-
-#2- open and reading compressed files 
-filepath= ""
- if filePath.endswith('.gz'):
-            self._file = gzip.open(filePath)
-        else:
-            self._file = open(filePath, 'rU')
-# counter to terminate reading from the file
-counter = 1
-# create a list to put reads from the file (i.e.list of SeqRead objects)
-reads = []
-# Open the file with keyword 'with'
-with open(filepath)as f:
-    # A loop that reads each 4 lines in the file as read object and puts them in a list of read objects
-    while True:
-        lines = []
-        for i in range(4):
-            lines.append(next(f))   # next brings the upcoming line
-counter += 1
+         else:
+            print("Invalid read".format(self.qual))        
+   
