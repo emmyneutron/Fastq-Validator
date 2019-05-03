c7114291df671a1a607ab608737a4a392b7b0314
Eman Abdelrazik <eman50371@student.must.edu.eg>
2019-05-03 23:32:58 +0200

    Update code.py

--- code.py
+++ code.py
@@ -15,9 +15,21 @@ class SeqRead:
     
  
 
-#2- open compressed files
+#2- open and reading compressed files 
 filepath= ""
  if filePath.endswith('.gz'):
             self._file = gzip.open(filePath)
         else:
             self._file = open(filePath, 'rU')
+# counter to terminate reading from the file
+counter = 1
+# create a list to put reads from the file (i.e.list of SeqRead objects)
+reads = []
+# Open the file with keyword 'with'
+with open(filepath)as f:
+    # A loop that reads each 4 lines in the file as read object and puts them in a list of read objects
+    while True:
+        lines = []
+        for i in range(4):
+            lines.append(next(f))   # next brings the upcoming line
+counter += 1
