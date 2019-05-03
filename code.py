73150d05c3512d67ccc02cc468613cc670845066
Eman Abdelrazik <eman50371@student.must.edu.eg>
2019-05-03 19:53:30 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,19 @@
 
+#1- Create a class named fastq
+class fastq(object):
+    seqHeader= "@" 
+    seq= ["A", "T", "C", "G", "N"]
+    plusline= "+"
+    qual= "c"
+    
+    def _init_(self, seqHeader, seq, plusline, qual):
+        print(seqHeader, seq, plusline, qual)
+
+#print(fastq.seqHeader)
+
+#2- open compressed files
+filepath= ""
+ if filePath.endswith('.gz'):
+            self._file = gzip.open(filePath)
+        else:
+            self._file = open(filePath, 'rU')
