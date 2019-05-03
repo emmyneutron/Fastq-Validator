15196e83145b419b4ce19251edff37b6a50a9e79
Eman Abdelrazik <eman50371@student.must.edu.eg>
2019-05-03 22:15:54 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,17 +1,15 @@
 
 #1- Create a class named fastq
 class SeqRead:
-def __init__(self, seqheader, seq, plus, qual):
+    def __init__(self, seqheader, seq, plusline, qual):
         self.id = seqheader
         self.seq = seq
         self.plus = plus
         self.qual = qual
-    len(seq)=len(qual)
+        
+    #len(seq)=len(qual)
     
-    def _init_(self, seqHeader, seq, plusline, qual):
-        print(seqHeader, seq, plusline, qual)
-
-#print(fastq.seqHeader)
+ 
 
 #2- open compressed files
 filepath= ""
