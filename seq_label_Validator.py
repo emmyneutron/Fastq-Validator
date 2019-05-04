09c90c16679022614c32e56a6a68e1665d853784
HowidaSamir <48625722+HowidaSamir@users.noreply.github.com>
2019-05-04 04:05:41 +0200

    Add files via upload

---
+++ seq_label_Validator.py
@@ -0,0 +1,5 @@
+def header_control (self):
+    if self.seqheader. startswith('@')
+        print('valid read'.format(self.seqheader))
+    else:
+        print('invalid read'.format(self.seqheader))
