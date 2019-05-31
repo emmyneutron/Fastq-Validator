635625da65506fb6b6f23429dddd85b21e1c49a8
Eman Abdelrazik <emmy_neutron18@yahoo.com>
2019-05-31 22:49:03 +0200

    Update Fastq Validator.py

--- Fastq Validator.py
+++ Fastq Validator.py
@@ -1,3 +1,4 @@
+import re
 # Create a class named SeqRead
 class SeqRead:
     # Initiate object in this class with its attributes
@@ -7,38 +8,24 @@ class SeqRead:
         self.plus = plus
         self.quality = qualit
 
-   # specify the path of your file
+# specify the path of your file
 file_path = r'E:\Bioinformatics_diploma\missing_plus_single.fq'
 
-
-def read_gz(self):  # open &read compressed file
+# open &read compressed file
+def read_gz(self):  
     filepath = ""
 
     if filepath.endswith('.gz'):
         self._file = gzip.open(filepath)
     else:
-        self._file = open(filepath, 'rU')
-
-    self._currentLineNumber = 0
-
-
-def next(self):
-    # Get Next Four Lines
-    elemList = []
-    for i in range(4):
-        line = self._file.readline()
-        self._currentLineNumber += 1  ## increment file position
-        if line:
-            elemList.append(line.strip('\n'))
-        else:
-            elemList.append(None)
+        self._file = open(filepath, 'r')
 
 # counter to terminate reading from the file
 counter = 1
 # create a list to put reads from the file (i.e.list of SeqRead objects)
 reads = []
-# Open the file with keyword 'with'
-with open(file_path)as f:
+# Open the file with keyword 'with', with open let file to be closed after iteration
+with open(file_path)as f, open("report.txt", 'w') as report::
     # A loop that reads each 4 lines in the file as read object and puts them in a list of read objects
     while True:
         lines = []
@@ -50,30 +37,37 @@ with open(file_path)as f:
         if counter >= 30:
             break
 
-
+#start analysis with checking header line starts with "@"
         def header_control(self):
             if self.seqheader.startswith('@'):
-                print('valid read'.format(self.seqheader))
+                report.write('valid read'.format(self.seqheader))
             else:
-                print('invalid read'.format(self.seqheader))
-
-
-        def test_no_plus_char_on_third_line(self):
+                report.write('invalid read'.format(self.seqheader))
+                
+#checking sequence line if it contains any other character other than  "ATCGNatcgn"
+         def seq_errors(self):
+            if re.match(r'[ATCGNatcgn]+$', self.seq) and len(self.seq) >= 36:
+                report.write('valid read'.format(self.seq))
+            else:
+                report.write('Invalid read'.format(self.seq))
+                
+#checking plus line if it contains + or not
+        def no_plus(self):
             if self.plus == '+':
-                print('valid read'.format(self.plus))
+                report.write('valid read'.format(self.plus))
             else:
-                print("Invalid read".format(self.plus))
+               report.write("Invalid read".format(self.plus))
+#checking quality line if its length of characters  = length of seuence line 
+         def qualine_errors(self):
+            if len(self.quality) == len(self.seq):
+                report.write('valid read'.format(self.quality))
+            else:
+                report.write("Invalid read".format(self.quality))
+print(lines)
+SeqRead.header_control()
+SeqRead.seq_errors()
+SeqRead.no_plus()
+SeqRead.qualine_errors()
 
 
-    def qualine_errors(self):
-        if len(self.quality) == len(self.seq):
-            print('valid read'.format(self.quality))
-        else:
-            print("Invalid read".format(self.quality))
 
-import re
-def seq_errors(self):
-    if re.match(r'[ATCGNatcgn]+$', self.seq) and len(self.seq) >= 36:
-        print('valid read'.format(self.seq))
-    else:
-        print('Invalid read'.format(self.seq))
