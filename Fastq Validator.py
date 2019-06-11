5fde218a65feadf648a40b84809d66a732ba501a
Eman Abdelrazik <emmy_neutron18@yahoo.com>
2019-06-11 15:51:26 +0200

    final update

--- Fastq Validator.py
+++ Fastq Validator.py
@@ -1,4 +1,6 @@
 import re
+import sys
+
 # Create a class named SeqRead
 class SeqRead:
     # Initiate object in this class with its attributes
@@ -7,64 +9,62 @@ class SeqRead:
         self.seq = seq
         self.plus = plus
         self.quality = qualit
-
-# specify the path of your file
-file_path = r'E:\Bioinformatics_diploma\missing_plus_single.fq'
-
+        
+#start analysis with checking header line starts with "@"
+    def header_control(self, report, line_no):
+        if not self.id.startswith('@'):
+            report.write('invalid header.{}\n'.format(line_no-3))
+                
+#checking sequence line if it contains any other character other than  "ATCGNatcgn"
+    def seq_errors(self,report, line_no,min_len):
+        if not re.match('[ATCGNatcgn]+$', self.seq) or len(self.seq) < min_len:
+            report.write('Invalid seq.{}\n'.format(line_no-2))
+                
+#checking plus line if it contains + or not
+    def no_plus(self,report, line_no):
+        if self.plus != '+\n':
+            report.write('Invalid plus_line.{}\n'.format(line_no-1))
+            
+#checking quality line if its length of characters  = length of sequence line 
+    def qualine_errors(self,report, line_no):
+        if len(self.quality) != len(self.seq):
+            report.write('Invalid quality.{}\n'.format(line_no))
+            
+            
 # open &read compressed file
-def read_gz(self):  
-    filepath = ""
-
-    if filepath.endswith('.gz'):
-        self._file = gzip.open(filepath)
+def read_gz(file_path):  
+    if file_path.endswith('.gz'):
+        file = gzip.open(file_path)
     else:
-        self._file = open(filepath, 'r')
+        file = open(file_path, 'r')
+    return file
+        
+# specify the path of your file
 
-# counter to terminate reading from the file
-counter = 1
-# create a list to put reads from the file (i.e.list of SeqRead objects)
-reads = []
+#input file 'fastq'
+file_path = sys.argv[1]
+min_len= int(sys.argv[2])
+
 # Open the file with keyword 'with', with open let file to be closed after iteration
-with open(file_path)as f, open("report.txt", 'w') as report::
+f= read_gz(file_path)
+#report_file= sys.argv[2]
+r_name = file_path + "report"
+
+counter=0
+with open(r_name, 'w') as report:
     # A loop that reads each 4 lines in the file as read object and puts them in a list of read objects
     while True:
         lines = []
         for i in range(4):
-            lines.append(next(f))   # next brings the upcoming line
-            counter += 1
-        # Create a read object named SeqRead
-        reads.append(SeqRead(read_id=lines[0], seq=lines[1], plus=lines[2], qualit=lines[3]))
-        if counter >= 30:
+            line= f.readline()
+            lines.append(line)  
+            counter +=1 
+        if not line:
             break
-
-#start analysis with checking header line starts with "@"
-        def header_control(self):
-            if self.seqheader.startswith('@'):
-                report.write('valid read'.format(self.seqheader))
-            else:
-                report.write('invalid read'.format(self.seqheader))
-                
-#checking sequence line if it contains any other character other than  "ATCGNatcgn"
-         def seq_errors(self):
-            if re.match(r'[ATCGNatcgn]+$', self.seq) and len(self.seq) >= 36:
-                report.write('valid read'.format(self.seq))
-            else:
-                report.write('Invalid read'.format(self.seq))
-                
-#checking plus line if it contains + or not
-        def no_plus(self):
-            if self.plus == '+':
-                report.write('valid read'.format(self.plus))
-            else:
-               report.write("Invalid read".format(self.plus))
-#checking quality line if its length of characters  = length of seuence line 
-         def qualine_errors(self):
-            if len(self.quality) == len(self.seq):
-                report.write('valid read'.format(self.quality))
-            else:
-                report.write("Invalid read".format(self.quality))
-print(reads)
-SeqRead.header_control()
-SeqRead.seq_errors()
-SeqRead.no_plus()
-SeqRead.qualine_errors()
+        # Create a read object named SeqRead
+        read= SeqRead(read_id=lines[0], seq=lines[1], plus=lines[2], qualit=lines[3])
+        read.header_control(report,counter)
+        read.seq_errors(report,counter,min_len)
+        read.no_plus(report,counter)
+        read.qualine_errors(report,counter)
+f.close()
