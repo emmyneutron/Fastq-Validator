f372be47363330c499e2d52bf610f9cc39f83263
Eman Abdelrazik <e.abdelrazik@nu.edu.eg>
2019-06-15 13:46:00 +0200

    Delete seq_label_Validator.py

--- seq_label_Validator.py
+++
@@ -1,5 +0,0 @@
-def header_control (self):
-    if self.seqheader. startswith('@')
-        print('valid read'.format(self.seqheader))
-    else:
-        print('invalid read'.format(self.seqheader))
