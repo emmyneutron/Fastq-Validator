dbfeb628eed1e8a1e4522a9f4e4dc93edf9611a7
Eman Abdelrazik <e.abdelrazik@nu.edu.eg>
2019-06-15 13:46:15 +0200

    Delete seq.py

--- seq.py
+++
@@ -1,6 +0,0 @@
-import re
-def seq_errors(self):
-    if re.match(r'[ATCGNatcgn]+$', self.seq) and len(self.seq) >= 36:
-        print('valid read'.format(self.seq))
-    else:
-        print('Invalid read'.format(self.seq))
